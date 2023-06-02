aa1bc94b84935b6d0625c97f5dd0cbcadf3ea803
mustafayusufozcan <mustafayusufozcan@gmail.com>
2023-06-03 00:47:55 +0300

    board updated

--- resources/views/livewire/sudoku/board.blade.php
+++ resources/views/livewire/sudoku/board.blade.php
@@ -1,7 +1,13 @@
-<div id="board">
-    @for ($x = 0; $x < count($sudoku); $x++)
-        @for ($y = 0; $y < count($sudoku[$x]); $y++)
-            @livewire('sudoku.block', ['x' => $x, "y" => $y, 'value' => $sudoku[$x][$y]])
+<div>
+    <div id="board">
+        @for ($x = 0; $x < count($sudoku); $x++)
+            @for ($y = 0; $y < count($sudoku[$x]); $y++)
+                @livewire('sudoku.block', ['x' => $x, "y" => $y, 'value' => $sudoku[$x][$y]["v"], 'status' => $sudoku[$x][$y]["s"]])
+            @endfor
         @endfor
-    @endfor
+    </div>
+    <div class="d-flex justify-content-center gap-1 mt-4">
+        <a href="javascript:;" class="btn btn-warning">New Game</a>
+        <a href="javascript:;" class="btn btn-success">Hint</a>
+    </div>
 </div>
