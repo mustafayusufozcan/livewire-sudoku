e9516309b07bafd0238917aec27251228f1ebf41
mustafayusufozcan <mustafayusufozcan@gmail.com>
2023-06-04 02:24:15 +0300

    Update board.blade.php

--- resources/views/livewire/sudoku/board.blade.php
+++ resources/views/livewire/sudoku/board.blade.php
@@ -2,12 +2,12 @@
     <div id="board">
         @for ($x = 0; $x < count($sudoku); $x++)
             @for ($y = 0; $y < count($sudoku[$x]); $y++)
-                @livewire('sudoku.block', ['x' => $x, "y" => $y, 'value' => $sudoku[$x][$y]["v"], 'status' => $sudoku[$x][$y]["s"]])
+                @livewire('sudoku.block', ['x' => $x, "y" => $y, 'value' => $sudoku[$x][$y]["v"], 'status' => $sudoku[$x][$y]["s"]], key(1000 . $x . $y))
             @endfor
         @endfor
     </div>
     <div class="d-flex justify-content-center gap-1 mt-4">
-        <a href="javascript:;" class="btn btn-warning">New Game</a>
+        <a href="javascript:;" wire:click="newGame" class="btn btn-warning">New Game</a>
         <a href="javascript:;" class="btn btn-success">Hint</a>
     </div>
 </div>
